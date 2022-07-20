b6ee0aa36328a13788d3272dad0a5a3e1cc6e953
Wong Meng Xian <mengxian98@gmail.com>
2022-07-20 12:33:31 +0800

    Update Callback URL

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ passport.use(new GoogleStrategy(
     {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/secrets",
+        callbackURL: "https://whispering-mesa-15829.herokuapp.com/auth/google/secrets",
         scope: ["profile", "email"]
     },
     (accessToken, refreshToken, profile, done) => {
@@ -113,7 +113,7 @@ passport.use(new FacebookStrategy(
     {
         clientID: process.env.FACEBOOK_APP_ID,
         clientSecret: process.env.FACEBOOK_APP_SECRET,
-        callbackURL: "http://localhost:3000/auth/facebook/secrets",
+        callbackURL: "https://whispering-mesa-15829.herokuapp.com/auth/facebook/secrets",
         profileFields: ["id", "email", "name"]
     },
     (accessToken, refreshToken, profile, done) => {
